ad2897f51951904630b435e4d6154b949d103c39
Keyrxng <106303466+Keyrxng@users.noreply.github.com>
2025-02-23 05:20:49 +0000

    feat: v1

---
+++ src/handlers/auto-response.ts
@@ -0,0 +1,26 @@
+import { Context } from "../types";
+
+export async function handleAutoResponse(context: Context) {
+  const {
+    logger,
+    payload,
+    config: { automatedResponses },
+    commentHandler,
+  } = context;
+
+  const repo = payload.repository.name;
+  const owner = payload.repository.owner.login;
+
+  let autoResponse = automatedResponses[`${owner}/${repo}`];
+  if (!autoResponse) {
+    autoResponse = automatedResponses[repo];
+  }
+
+  if (!autoResponse) {
+    autoResponse = automatedResponses[owner];
+  }
+
+  if (autoResponse) {
+    await commentHandler.postComment(context, logger.warn(autoResponse, { owner, repo }));
+  }
+}
